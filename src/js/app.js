d8006a9f3c326b7c96471d6edacf073cbf7a338c
sedoy113 <sedoy113@yandex.ru>
2021-06-20 19:44:40 +0300

    clear

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,4 @@
-// import * as logs from './logs';
-// import * as stats from './stats';
-// import moment from 'moment';
 import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
-// import('./game.js');
+
 const game = new Game();
 game.start();
